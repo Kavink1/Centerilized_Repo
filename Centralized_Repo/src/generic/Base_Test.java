15fa84a13e3d5d559bff50c3d9b30d0211ff0b60
kavin k <kavin k@DESKTOP-EBLQ19S>
2024-09-26 22:23:04 +0530

    added genric webdriver

--- Centralized_Repo/src/generic/Base_Test.java
+++ Centralized_Repo/src/generic/Base_Test.java
@@ -1,9 +1,11 @@
 package generic;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 
 public class Base_Test 
 {
+	public static WebDriver driver; 
 	@BeforeMethod
 	public void ssOpenApp()
 	{
